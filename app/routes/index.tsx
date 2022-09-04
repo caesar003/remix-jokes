098a9697bff8353dbe34e975b8c5ce639bfe3704
caesar003 <caesarmuksid@gmail.com>
2022-09-04 17:36:00 +0700

    jokes route

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,11 @@
 export default function IndexRoute(){
-    return <div>Hello Index!</div>
+    return (
+        <div>
+            <p>Here's a random joke</p>
+            <p>
+                I wa wondering why the frisbee was getting bigger,
+                then it hit me.
+            </p>
+        </div>
+    )
 }
